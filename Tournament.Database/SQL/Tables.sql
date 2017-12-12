3b343c114be03bd73bd754a2ebfa0b033f27068d
VIRTUAL\lucas.monteiro <lucas.monteiro@sellbie.com.br>
2017-12-12 18:10:19 -0200

    Add players table

--- Tournament.Database/SQL/Tables.sql
+++ Tournament.Database/SQL/Tables.sql
@@ -34,3 +34,17 @@ CREATE TABLE Match(
 	CONSTRAINT FK_Match_Team2 FOREIGN KEY (Team2) REFERENCES Tournament (TeamId),
 	CONSTRAINT FK_Match_Winner FOREIGN KEY (Winner) REFERENCES Tournament (TeamId)
 )
+
+
+CREATE TABLE Player(
+
+	PlayerId bigint IDENTITY(1,1),
+	[Name] VARCHAR(200),
+	BirthDate DATETIME,
+	Genre VARCHAR(1),
+	TeamId bigint,
+
+	CONSTRAINT PK_Player PRIMARY KEY (PlayerId),
+	CONSTRAINT FK_Player_Team FOREIGN KEY (TeamId) REFERENCES Tournament (TeamId),
+	CONSTRAINT CHK_Player_Sex CHECK (Genre = 'H' or Genre = 'M')
+)
